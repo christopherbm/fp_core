f9e62f2bb82c492c4f722846c321f653757149c0
Chris Myers <christopherbm@gmail.com>
2019-04-18 12:38:28 -0700

    update index

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Core functional programming logic in Javascript. Inspired by Clojure and Rust.
 
 The focus of this library is functional composition, polymorphism, purposeful mutability, and
-monads
+monads.
--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 "use strict";
 module.exports =  
 {
-  'has': require("./src/utils/has")(),
+	"null_apply": require("./src/fns/null_apply")(),
+  
+	"is_err": require("./src/monads/is_err")(),
+	"is_none": require("./src/monads/is_none")(),
+	"is_ok": require("./src/monads/is_ok")(),
+	"is_some": require("./src/monads/is_some")(),
+	"match_result": require("./src/monads/match_result")(), // deprecate and replace with cond
+	"option_none": require("./src/monads/option_none")(),
+	"option_some": require("./src/monads/option_some")(),
+	"option_to_result": require("./src/monads/option_to_result")(),
+	"OptionMonad": require("./src/monads/OptionMonad")(),
+	"result_err": require("./src/monads/result_err")(),
+	"result_ok": require("./src/monads/result_ok")(),
+	"result_to_option": require("./src/monads/result_to_option")(),
+	"ResultMonad": require("./src/monads/ResultMonad")(),
+	"unwrap": require("./src/monads/unwrap")(),
+	"unwrap_or": require("./src/monads/unwrap_or")(),
+	"unwrap_or_else": require("./src/monads/unwrap_or_else")(),
+	"unwrap_to": require("./src/monads/is_none")(),
+	"unwrap_to_or": require("./src/monads/unwrap_to_or")(),
+	"wrap_with_monad": require("./src/monads/wrap_with_monad")(),
+
+  "args_to_array": require("./src/utils/args_to_array")(),
+  "freeze_and_seal": require("./src/utils/freeze_and_seal")(),
+  "has": require("./src/utils/has")(),
 }
