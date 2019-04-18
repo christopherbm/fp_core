e087665353f519f68e1b8b116f78dffeb7f8e713
Chris Myers <christopherbm@gmail.com>
2019-04-18 10:14:58 -0700

    utils

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fp_core
 Core functional programming logic in Javascript. Inspired by Clojure and Rust.
+
---
+++ src/fns/null_apply.js
@@ -0,0 +1,18 @@
+"use strict";
+module.exports = function (options) 
+{
+  const args_to_array = require("./utils/args_to_array")();
+  
+  /**
+   * Calls apply on the given function using null as "this" and passing the given args object as 
+   *   an array.
+   * @param {Function} fn - Function to call apply on.
+   * @param {Arguments} args - Arguments object used in apply (converted to array first).
+   * @returns {Any} - Returns fn return value.
+   */
+  function null_apply (fn, args) {
+    return fn.apply(null, args_to_array(args));
+  }
+
+  return null_apply;
+}
